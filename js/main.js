23ad3effb527eadb21146a009147544ad265ac82
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-05-22 02:31:06 +0300

    menu button functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const header = document.getElementById("header");
 const navMenu = document.getElementById("navMenu");
 const navLinks = document.querySelectorAll(".nav__a");
+const navButton = document.getElementsByClassName("navbar__button")[0];
 const sections = document.querySelectorAll("section");
 
 document.documentElement.style.setProperty(
@@ -11,26 +12,36 @@ document.documentElement.style.setProperty(
 window.addEventListener("scroll", () => {
   let scrollPosition = window.scrollY + header.offsetHeight;
 
-  // adding header__scrolled class incase of scrolling past 0
-  if (window.scrollY != 0) {
-    header.classList.add("header__scrolled");
+  // adding header--white class incase of scrolling past 0
+  if (window.scrollY != 0 || window.visualViewport.width <= 991) {
+    header.classList.add("header--white");
   } else {
-    header.classList.remove("header__scrolled");
+    header.classList.remove("header--white");
   }
 
   // updating navbar based on current section
   sections.forEach((section) => {
     if (scrollPosition >= section.offsetTop) {
       navLinks.forEach((link) => {
-        console.log("inside scrollposition");
         link.classList.remove("active");
         if (
           section.getAttribute("id") === link.getAttribute("href").substring(1)
         ) {
-          console.log("inside equality");
           link.classList.add("active");
         }
       });
     }
   });
 });
+
+// menu toggler
+if (window.visualViewport.width <= 991) {
+  header.classList.add("header--white");
+  window.addEventListener("click", (event) => {
+    if (event.target === navButton || event.target === navButton.children[0]) {
+      navMenu.classList.toggle("verticle-menu");
+    } else if (navMenu.classList.contains("verticle-menu")) {
+      navMenu.classList.remove("verticle-menu");
+    }
+  });
+}
