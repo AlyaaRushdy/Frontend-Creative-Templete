25074f56c41a63626f6a486be3bfe14d77cf4555
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-05-21 07:28:04 +0300

    updating navbar based on the viewed section JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const header = document.getElementById("header");
+const navMenu = document.getElementById("navMenu");
+const navLinks = document.querySelectorAll(".nav__a");
+const sections = document.querySelectorAll("section");
 
 document.documentElement.style.setProperty(
   "--header-height",
@@ -6,10 +9,28 @@ document.documentElement.style.setProperty(
 );
 
 window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY + header.offsetHeight;
+
   // adding header__scrolled class incase of scrolling past 0
   if (window.scrollY != 0) {
     header.classList.add("header__scrolled");
   } else {
     header.classList.remove("header__scrolled");
   }
+
+  // updating navbar based on current section
+  sections.forEach((section) => {
+    if (scrollPosition >= section.offsetTop) {
+      navLinks.forEach((link) => {
+        console.log("inside scrollposition");
+        link.classList.remove("active");
+        if (
+          section.getAttribute("id") === link.getAttribute("href").substring(1)
+        ) {
+          console.log("inside equality");
+          link.classList.add("active");
+        }
+      });
+    }
+  });
 });
