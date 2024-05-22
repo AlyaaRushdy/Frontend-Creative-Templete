38023a6a4d6abf8f9fde2fd880f02128cb4b13a2
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-05-22 03:15:31 +0300

    adding portfolio images dynamically

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
+const dir = "./images/portfolio";
 const header = document.getElementById("header");
 const navMenu = document.getElementById("navMenu");
 const navLinks = document.querySelectorAll(".nav__a");
 const navButton = document.getElementsByClassName("navbar__button")[0];
 const sections = document.querySelectorAll("section");
+const imageContainer = document.getElementsByClassName(
+  "portfolio_image-container"
+)[0];
+let imageELement = "";
 
 document.documentElement.style.setProperty(
   "--header-height",
   `${header.offsetHeight}px`
 );
+
+for (let i = 0; i < 6; i++) {
+  let image = new Image();
+  image.setAttribute("src", `${dir}/${i + 1}.jpg`);
+  image.classList.add("portfolio__img");
+  image.setAttribute("class", "portfolio__img");
+  imageELement += image.outerHTML;
+}
+imageContainer.innerHTML = imageELement;
 
 window.addEventListener("scroll", () => {
   let scrollPosition = window.scrollY + header.offsetHeight;
